0b2d98091969732149d9f9720aaa091173c8b0c8
thunderdbolt <135745865+thunderdbolt@users.noreply.github.com>
2024-04-08 01:05:17 -0600

    Update tariffs.py

--- tariffs.py
+++ tariffs.py
@@ -55,8 +55,7 @@ with col2:
 with col3:
     st.markdown(
         # f'<a href="{slb_website_url}">'
-        f'<img src="{mid_image_url}" alt="Middle Image" style="max-height:{max_image_height}; width:93%;">'
-        f'</a>',
+        f'<img src="{mid_image_url}" alt="Middle Image" style="max-height:{max_image_height}; max-width:100%;"></a>',
         unsafe_allow_html=True,
     )
     
