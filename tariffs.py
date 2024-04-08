9a7f8dc5e9da40685308a58c2d577930e16e7c93
thunderdbolt <135745865+thunderdbolt@users.noreply.github.com>
2024-04-08 00:50:04 -0600

    Update tariffs.py

--- tariffs.py
+++ tariffs.py
@@ -17,7 +17,7 @@ pipesim_logo_url = "https://americanpatriotsdecals.com/cdn/shop/products/America
 mid_image_url = "https://images.rigzone.com/images/news/articles/SLB-Bags-5Year-Digital-Deal-with-Petrobras-173287-1200x900.webp"
 seal_award_image_url = "https://cei.org/wp-content/uploads/2017/08/2000px-Seal_of_the_United_States_Department_of_Commerce.svg_.png"
 seal_award_website_url = "https://dataweb.usitc.gov/"
-ghg_image_url = "https://lot.dhl.com/wp-content/uploads/2019/05/res_shutterstock_377010349-800x420.jpg"
+ghg_image_url = "https://www.gcsrfid.com/wp-content/uploads/2018/08/trucks.png"
 # ghg_website_url = "https://www.iogp.org/blog/news/how-the-oil-and-gas-industry-contributes-to-a-lower-carbon-future/"
 
 # Custom CSS to remove padding and margin from columns
